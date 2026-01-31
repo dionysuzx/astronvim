4258efd0cefb3bcdee27f2ca17ee4c9918d35a52
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2026-01-31 15:27:14 -0500

    Disable indent guides for markdown files

--- lua/plugins/fixes.lua
+++ lua/plugins/fixes.lua
@@ -1,4 +1,20 @@
 return {
   -- Disable vim-illuminate (word highlighting under cursor)
   { "RRethy/vim-illuminate", enabled = false },
+
+  -- Disable indent guides for markdown
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      autocmds = {
+        disable_indent_markdown = {
+          {
+            event = "FileType",
+            pattern = "markdown",
+            callback = function() vim.b.snacks_indent = false end,
+          },
+        },
+      },
+    },
+  },
 }
