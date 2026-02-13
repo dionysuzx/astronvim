a37ed55d9b59e5526f1fa52c1ca1f5f7fee812bf
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2026-02-13 12:22:28 -0500

    Force OSC 52 clipboard provider over xsel in Lima VM

--- lua/plugins/options.lua
+++ lua/plugins/options.lua
@@ -6,6 +6,19 @@ return {
         wrap = true,
         clipboard = "unnamedplus",
       },
+      g = {
+        clipboard = {
+          name = "OSC 52",
+          copy = {
+            ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+            ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+          },
+          paste = {
+            ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+            ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+          },
+        },
+      },
     },
   },
 }
